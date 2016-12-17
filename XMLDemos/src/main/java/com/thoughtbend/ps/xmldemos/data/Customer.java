37002c08c55d1921d06f957d672fe1f019c83149
Mike Nolan <michael.nolan@parivedasolutions.com>
2016-12-17 10:15:46 -0600

    Base project checkin

---
+++ XMLDemos/src/main/java/com/thoughtbend/ps/xmldemos/data/Customer.java
@@ -0,0 +1,53 @@
+package com.thoughtbend.ps.xmldemos.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private List<Address> addresses = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+}
