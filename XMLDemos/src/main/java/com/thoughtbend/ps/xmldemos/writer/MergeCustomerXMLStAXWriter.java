89489dfca4cedf71c79a21d587a1522f6f589f40
Mike Nolan <michael.nolan@parivedasolutions.com>
2017-01-21 09:33:25 -0800

    Updates for completed merge demo

--- XMLDemos/src/main/java/com/thoughtbend/ps/xmldemos/writer/MergeCustomerXMLStAXWriter.java
+++ XMLDemos/src/main/java/com/thoughtbend/ps/xmldemos/writer/MergeCustomerXMLStAXWriter.java
@@ -7,12 +7,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.xml.namespace.QName;
+import javax.xml.stream.EventFilter;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
 public class MergeCustomerXMLStAXWriter {
@@ -24,13 +28,16 @@ public class MergeCustomerXMLStAXWriter {
 			 final OutputStream fos = new FileOutputStream("./merged-customers.xml")) {
 			
 			XMLEventFactory outputEventFactory = XMLEventFactory.newFactory();
+			
 			XMLEventWriter mergedCustomerEventWriter = XMLOutputFactory.newFactory().createXMLEventWriter(System.out);
 			
 			XMLEvent rootDocumentElement = outputEventFactory.createStartDocument();
 			mergedCustomerEventWriter.add(rootDocumentElement);
 			
 			XMLEvent customersStartElement = outputEventFactory.createStartElement("tbc", CUSTOMER, "customers");
+			XMLEvent customersNamespace = outputEventFactory.createNamespace("tbc", CUSTOMER);
 			mergedCustomerEventWriter.add(customersStartElement);
+			mergedCustomerEventWriter.add(customersNamespace);
 			
 			filterCustomersDocAndWrite(mergedCustomerEventWriter, xmlStream1);
 			filterCustomersDocAndWrite(mergedCustomerEventWriter, xmlStream2);
@@ -51,7 +58,40 @@ public class MergeCustomerXMLStAXWriter {
 		XMLInputFactory inputFactory = XMLInputFactory.newFactory();
 		XMLEventReader eventReader = inputFactory.createXMLEventReader(xmlStream);
 		
-		mergedCustomerEventWriter.add(eventReader);
+		// On first attempt, ensure you are not using the reader - we want the failure to show the mixed structure
+		// of simply merging two files.
+		XMLEventReader filteredEventReader = inputFactory.createFilteredReader(eventReader, new EventFilter() {
+			
+			boolean inCustomer = false;
+			
+			@Override
+			public boolean accept(XMLEvent event) {
+				
+				if (event.isStartElement()) {
+				
+					StartElement startElement = event.asStartElement();
+					QName name = startElement.getName();
+					if (CUSTOMER.equals(name.getNamespaceURI()) && "customer".equals(name.getLocalPart())) {
+						inCustomer = true;
+					}
+				}
+				
+				if (event.isEndElement()) {
+					
+					EndElement endElement = event.asEndElement();
+					QName name = endElement.getName();
+					// This needs to be an element after "customer" to ensure the closing tag is included, so we know once we reach the first customer we want to keep going 
+					// until that end element
+					if (CUSTOMER.equals(name.getNamespaceURI()) && "customers".equals(name.getLocalPart())) {
+						inCustomer = false;
+					}
+				}
+				
+				return inCustomer;
+			}
+		});
+		
+		mergedCustomerEventWriter.add(filteredEventReader);
 		mergedCustomerEventWriter.flush();
 	}
 
